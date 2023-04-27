487f5299facdaf47c3a0ff58f6b101c439c052b8
ajundcruzz <arul.arulraj@fwsa.freshworks.com>
2023-04-27 17:20:51 +0530

    add js

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+//Dire
+
+let number_id = JSON.parse(localStorage.getItem("number_id"));
+let usrjoin = document.getElementById('usrjoin');
+document.getElementById('usrjoin').onclick = function () {
+    if (number_id == null) {
+        window.location.href = './pages/CLIENT/clientloginpage.html';
+    } else {
+        window.location.href = './pages/CLIENT/client main page.html';
+    }
+};
